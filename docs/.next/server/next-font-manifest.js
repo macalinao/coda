6a37b3624e5e1698453dc3330bb695fe71cce5b3
Ian Macalinao <github@igm.pub>
2025-08-18 19:48:10 +0800

    updates

--- docs/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/igm/proj/macalinao/coda/docs/src/app/layout\":[\"static/media/e4af272ccee01ff0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
