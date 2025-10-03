d83c8289ac2c19f032672818f69e969ecb11091f
Ian Macalinao <github@igm.pub>
2025-10-03 09:33:59 +0800

    Adds SPL governance client

---
+++ clients/spl-governance/src/index.ts
@@ -0,0 +1,8 @@
+/**
+ * @macalinao/clients-spl-governance
+ *
+ * TypeScript client for the SPL Governance program.
+ * Generated using Codama with full ESM support.
+ */
+
+export * from "./generated/index.js";
