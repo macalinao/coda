5a2af727bc0a029c24a683e06268a29f27add337
Ian Macalinao <github@igm.pub>
2025-10-03 11:28:24 +0800

    fix typedoc generation

---
+++ packages/create-coda/src/index.ts
@@ -0,0 +1,6 @@
+/**
+ * This package does not export anything. It is used to scaffold new Coda client projects.
+ *
+ * Use `bun create @macalinao/create-coda my-client` to scaffold a new project.
+ */
+export {};
